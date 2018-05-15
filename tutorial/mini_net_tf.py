29ddb8bab46de3f41a303120633827d761b68b4a
fengtaolue <fengtaolue@xiaomi.com>
2018-05-15 19:10:00 +0800

    grads

--- tutorial/mini_net_tf.py
+++ tutorial/mini_net_tf.py
@@ -13,12 +13,20 @@ def network_fn(input_x):
 
 
 # 模型, 返回train_op. 为了方便查看参数, 会把参数也返回.
+# def model_fn(input_x, label):
+#     pred, (_weight, _bias) = network_fn(input_x)
+#     loss = tf.losses.mean_squared_error(label, pred)
+#     train_op = tf.train.GradientDescentOptimizer(0.01).minimize(loss)
+#     # train_op = tf.train.AdamOptimizer(0.01).minimize(loss)
+#     return train_op, loss, (_weight, _bias)
 def model_fn(input_x, label):
     pred, (_weight, _bias) = network_fn(input_x)
     loss = tf.losses.mean_squared_error(label, pred)
-    train_op = tf.train.GradientDescentOptimizer(0.01).minimize(loss)
+    optimizer = tf.train.GradientDescentOptimizer(0.01)
+    grads = optimizer.compute_gradients(loss)
+    train_op = optimizer.apply_gradients(grads)
     # train_op = tf.train.AdamOptimizer(0.01).minimize(loss)
-    return train_op, loss, (_weight, _bias)
+    return train_op, loss, grads, (_weight, _bias)
 
 
 def main(_):
@@ -26,7 +34,7 @@ def main(_):
     x = tf.constant([[1.0, 2.0]])
     y = tf.constant([[1.0]])
 
-    train_op, loss, (_weight, _bias) = model_fn(x, y)
+    train_op, loss, grads, (_weight, _bias) = model_fn(x, y)
     # 注意这一行必须要在train_op后面, 否则会有错误Attempting to use uninitialized value weight.
     # 详细可以看这个链接 https://github.com/tensorflow/tensorflow/issues/8057#issuecomment-310505897
     init_op = tf.global_variables_initializer()
@@ -38,7 +46,8 @@ def main(_):
         for i in range(3):
             sess.run(train_op)
             print("W[%d]:\n %s\nb[%d]:%s" % (i+1, sess.run(_weight), i+1, sess.run(_bias)))
-            print("loss[%d]:%s\n" % (i+1, sess.run(loss)))
+            print("loss[%d]:%s" % (i+1, sess.run(loss)))
+            print("Grads[%d]:%s\n" % (i+1, sess.run(grads)))
 
 
 if __name__ == "__main__":
